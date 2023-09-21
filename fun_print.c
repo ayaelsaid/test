180cecf29ee4fd1c7e0d715f1fac0251a44e4f07
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2023-09-21 03:47:29 +0300

    Create fun_print.c

---
+++ fun_print.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * sp_puts - function that prints a string
+ * @arg: va_list containing the string
+ * Return: characte]rs number
+ */
+int sp_puts(va_list arg)
+{
+        const char *str = va_arg(arg, char *);
+        int char_count = 0;
+        int i = 0;
+
+        if (!str)
+        {
+                str = "(null)";
+
+                while (str[i] != '\0')
+                {
+                        _putchar(str[i]);
+                        char_count++;
+                        i++;
+                }
+        }
+        else
+        {
+                i = 0;
+                while (str[i] != '\0')
+                {
+                        _putchar(str[i]);
+                        char_count++;
+                        i++;
+                }
+        }
+        return (char_count);
+}
