676f75ccbb8d4cef0f46ce2d41bd37d8ab2465c7
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2023-09-21 03:54:07 +0300

    fun_print.c

--- fun_print.c
+++ fun_print.c
@@ -7,30 +7,30 @@
  */
 int sp_puts(va_list arg)
 {
-        const char *str = va_arg(arg, char *);
-        int char_count = 0;
-        int i = 0;
+	const char *str = va_arg(arg, char *);
+	int char_count = 0;
+	int i = 0;
 
-        if (!str)
-        {
-                str = "(null)";
+	if (!str)
+	{
+		str = "(null)";
 
-                while (str[i] != '\0')
-                {
-                        _putchar(str[i]);
-                        char_count++;
-                        i++;
-                }
-        }
-        else
-        {
-                i = 0;
-                while (str[i] != '\0')
-                {
-                        _putchar(str[i]);
-                        char_count++;
-                        i++;
-                }
-        }
-        return (char_count);
+		while (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			char_count++;
+			i++;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			char_count++;
+			i++;
+		}
+	}
+	return (char_count);
 }
